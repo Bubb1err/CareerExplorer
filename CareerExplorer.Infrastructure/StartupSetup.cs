405e9bf5b64de496502e1ecea0dab79ec7958d8d
Bubbler <104278936+Bubb1err@users.noreply.github.com>
2023-03-16 19:37:54 +0200

    connectDb

---
+++ CareerExplorer.Infrastructure/StartupSetup.cs
@@ -0,0 +1,18 @@
+﻿using CareerExplorer.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerExplorer.Infrastructure
+{
+    public static class StartupSetup
+    {
+        public static void AddDbContext(this IServiceCollection services, string connectionString) =>
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(connectionString)); 
+    }
+}
